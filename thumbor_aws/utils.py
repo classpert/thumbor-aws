305a8b70195b0bd1aedf93e8986cf2809895ee07
Thiago Brandão <194487+0xradical@users.noreply.github.com>
2023-01-13 11:16:14 -0300

    Update utils.py
    Don't unquote

--- thumbor_aws/utils.py
+++ thumbor_aws/utils.py
@@ -5,7 +5,7 @@ def normalize_path(prefix: str, path: str) -> str:
     # Thumbor calls load/store functions with a URL that is  URL-encoded and
     # that always starts with a slash. S3Client doesn't expect URL-encoding
     # and S3 keys don't usually start with a slash. Decode and remove slash.
-    path = unquote(path).lstrip("/")
+    path = path.lstrip("/")
     if prefix:
         # Avoid double slash if prefix ends with a slash.
         prefix = prefix.rstrip("/")
